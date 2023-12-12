a7927b9dd0be19dbd09145be30de273ba174e86d
tesar <tesarek@dnservis.cz>
2023-12-12 10:10:56 +0100

    basic bootstrap template

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -14,7 +14,7 @@ class Bootstrap
 		$configurator = new Configurator;
 		$appDir = dirname(__DIR__);
 
-		//$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
+		$configurator->setDebugMode('31.47.102.92'); // enable for your remote IP
 		$configurator->enableTracy($appDir . '/log');
 
 		$configurator->setTempDirectory($appDir . '/temp');
--- app/Presenters/HomePresenter.php
+++ app/Presenters/HomePresenter.php
@@ -9,4 +9,10 @@ use Nette;
 
 final class HomePresenter extends Nette\Application\UI\Presenter
 {
+    public function renderDefault()
+    {
+        if ($_SERVER['REMOTE_ADDR'] !== '31.47.102.92') {
+            $this->redirectUrl('http://localhost');
+        }
+    }
 }
